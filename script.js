d9bbb123befcdd4395d96e13848d4125f3d0b7e3
Tokenph <moisesbarbosa23@gmail.com>
2021-08-30 11:13:45 -0300

    Feito o HTML, CSS e também a primeira parte da calculadora com JS.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const bill = document.querySelector('.dollar-input'); 
 const people = document.querySelector('.person-input')
 const buttons = document.querySelectorAll('button');
-buttons.forEach((item) => {
-  item.classList.remove('active');
-  item.addEventListener('click', function() {
-    item.classList.toggle('active');
+const tipValue = document.querySelector('.tipTotalValue')
+const finalValue = document.querySelector('.totalValue')
+console.log(finalValue);
+
+function tipCalculator(index) {
+  buttons.forEach((item) => {
+    item.classList.remove('active');
   })
-})
+  buttons[index].classList.add('active');
+  let tipTotal = parseFloat(bill.value) * parseFloat(buttons[index].value) / parseFloat(people.value);
+  let totalValue = (parseFloat(tipTotal) + parseFloat(bill.value))
+  tipValue.innerHTML = 'U$ ' + tipTotal;
+  finalValue.innerHTML = 'U$ ' + totalValue;
+}
+  buttons.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      tipCalculator(index);
+    })
+  })
