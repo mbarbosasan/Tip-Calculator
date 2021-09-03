2e61210a38b0b6a6888f1011ea459d7ca18ea293
Tokenph <moisesbarbosa23@gmail.com>
2021-09-02 21:26:23 -0300

    Reorganizado o script.js e adicionado mais algumas estilizações

--- script.js
+++ script.js
@@ -3,20 +3,39 @@ const people = document.querySelector('.input-value')
 const buttons = document.querySelectorAll('button');
 const tipValue = document.querySelector('.tipTotalValue')
 const finalValue = document.querySelector('.totalValue')
-console.log(finalValue);
+const inputValue = document.querySelector('.percentage-manual')
 
-function tipCalculator(index) {
+function disableButton() {
+  
+}
+
+buttons.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    activeButton(index);
+  })
+})
+
+function activeButton(index) {
+//Ativando e desativando botoões
   buttons.forEach((item) => {
     item.classList.remove('active');
   })
   buttons[index].classList.add('active');
-  let tipTotal = parseFloat(bill.value) * parseFloat(buttons[index].value) / parseFloat(people.value);
-  let totalValue = (parseFloat(tipTotal) + parseFloat(bill.value))
-  tipValue.innerHTML = 'U$ ' + tipTotal;
-  finalValue.innerHTML = 'U$ ' + totalValue;
 }
-  buttons.forEach((item, index) => {
-    item.addEventListener('click', () => {
-      tipCalculator(index);
-    })
+
+
+function tipCalculator() {
+  buttons.forEach((item) => {
+    if (item.className === 'active') {
+      if (item.value === '0') {
+
+      }
+      // var buttonActive = item.value;
+        let tipTotal = parseFloat(bill.value) * (parseFloat(inputValue.value / 100)) + parseFloat(item.value / 100)
+        / parseFloat(people.value);
+        let totalValue = (parseFloat(bill.value) / parseFloat(people.value)) + parseFloat(tipTotal);
+        tipValue.innerHTML = '$' + tipTotal.toFixed(2);
+        finalValue.innerHTML = '$' + totalValue.toFixed(2);
+    }
   })
+}
